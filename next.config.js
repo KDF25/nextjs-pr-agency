2cece3bb83fdc9e17ec67bab2fad576f386fc5f0
KDF25 <karimov.damir.faridovich@gmail.com>
2024-10-23 11:05:03 +0500

    little fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 // next.config.js
 
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
-    domains: [
-      "static.wixstatic.com",
-      "venconbucket.s3.eu-north-1.amazonaws.com",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "static.wixstatic.com",
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: "venconbucket.s3.eu-north-1.amazonaws.com",
+        port: '',
+      },
     ],
   },
 };
+
 
 module.exports = nextConfig;
